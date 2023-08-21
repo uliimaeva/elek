f2c2b5434a183b9062745141c0d9bb32da181487
work <i.julka14@gmail.com>
2023-08-21 15:09:03 +0300

    layout update + add functions

--- app/src/main/java/pp/dair/activities/MainActivity.kt
+++ app/src/main/java/pp/dair/activities/MainActivity.kt
@@ -6,6 +6,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.MenuItem
+import android.view.View
+import android.widget.DatePicker
 import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.TextView
@@ -14,6 +16,7 @@ import androidx.appcompat.widget.Toolbar
 import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.view.menu.ActionMenuItemView
 import androidx.core.app.ActivityCompat
+import androidx.core.view.isVisible
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import pp.dair.R
@@ -23,6 +26,7 @@ import pp.dair.viewmodels.MarksViewModel
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import java.time.DayOfWeek
 import java.util.Calendar
 import kotlin.system.exitProcess
 
@@ -37,40 +41,58 @@ class MainActivity : AppCompatActivity() {
     private lateinit var visit_line: ImageView
     private lateinit var journalButton: ActionMenuItemView
 
-    inner class ScheduleLoader: Runnable {
-        override fun run() {
-            val day = calendar.get(Calendar.DAY_OF_MONTH)
-            val year = calendar.get(Calendar.YEAR)
-            val month = calendar.get(Calendar.MONTH) + 1
-            val day_of_year = calendar.get(Calendar.DAY_OF_YEAR)
-            header.text = String.format("%d.%d.%d %s", day, month, year, numberToWeekDay(calendar.get(Calendar.DAY_OF_WEEK)))
-            Thread.sleep(500)
-            if (calendar.get(Calendar.DAY_OF_YEAR) != day_of_year) {
-                return;
-            }
-            viewModel.getDaySchedule(year, month, day, object: Callback<ArrayList<LessonWithMark>> {
-                override fun onResponse(
-                    call: Call<ArrayList<LessonWithMark>>,
-                    response: Response<ArrayList<LessonWithMark>>
-                ) {
-                    if (response.isSuccessful) {
-                        Log.d("HUI", "Расписание получено!")
-                        for (lesson in response.body()!!) {
-                            Log.d("HUI", lesson.toString())
-                        }
-                        adapter.setArray(response.body()!!)
-                    } else {
-                        showToast("Технические шоколадки 2")
-                        adapter.setArray(ArrayList())
-                    }
-                }
+    private lateinit var datePicker: DatePicker
+    private lateinit var calendarButton: ImageButton
 
-                override fun onFailure(call: Call<ArrayList<LessonWithMark>>, t: Throwable) {
-                    adapter.setArray(ArrayList())
-                    showToast("Технические шоколадки!")
-                }
-            })
+
+    var day = calendar.get(Calendar.DAY_OF_MONTH)
+    var year = calendar.get(Calendar.YEAR)
+    var month = calendar.get(Calendar.MONTH) + 1
+    var day_of_year = calendar.get(Calendar.DAY_OF_YEAR)
+
+
+    fun setData() {
+        val today = Calendar.getInstance()
+        val date = Calendar.getInstance()
+
+        datePicker.init(today.get(Calendar.YEAR), today.get(Calendar.MONTH),
+            today.get(Calendar.DAY_OF_MONTH)
+
+        ) { _, year, month, day ->
+            val month = month + 1
+            date.set(year, month, day)
+            header.text = String.format("%d.%d.%d %s", day, month, year, numberToWeekDay(date.get(Calendar.DAY_OF_WEEK)))
+            this.day = datePicker.dayOfMonth
+            this.year = datePicker.year
+            this.month = datePicker.month
         }
+
+    }
+
+    fun loadSchedule() {
+        header.text = String.format("%d.%d.%d %s", day, month, year, numberToWeekDay(calendar.get(Calendar.DAY_OF_WEEK)))
+        viewModel.getDaySchedule(year, month, day, object: Callback<ArrayList<LessonWithMark>> {
+            override fun onResponse(
+                call: Call<ArrayList<LessonWithMark>>,
+                response: Response<ArrayList<LessonWithMark>>
+            ) {
+                if (response.isSuccessful) {
+                    Log.d("HUI", "Расписание получено!")
+                    for (lesson in response.body()!!) {
+                        Log.d("HUI", lesson.toString())
+                    }
+                    adapter.setArray(response.body()!!)
+                } else {
+                    showToast("Технические шоколадки 2")
+                    adapter.setArray(ArrayList())
+                }
+            }
+
+            override fun onFailure(call: Call<ArrayList<LessonWithMark>>, t: Throwable) {
+                adapter.setArray(ArrayList())
+                showToast("Технические шоколадки!")
+            }
+        })
     }
 
     fun numberToWeekDay(value: Int): String {
@@ -111,21 +133,42 @@ class MainActivity : AppCompatActivity() {
         journalButton = menu.findViewById(R.id.journal)
         journalButton.setOnClickListener { openJournal() }
         header = findViewById(R.id.r_date)
+        datePicker = findViewById(R.id.datePicker1)
+        calendarButton = findViewById(R.id.calendar)
         left_button = findViewById(R.id.left)
+        right_button = findViewById(R.id.right)
+
         left_button.setOnClickListener {
             calendar.add(Calendar.DAY_OF_MONTH, -1)
-            Thread(ScheduleLoader()).start()
+            loadSchedule()
         }
-        right_button = findViewById(R.id.right)
         right_button.setOnClickListener {
             calendar.add(Calendar.DAY_OF_MONTH, 1)
-            Thread(ScheduleLoader()).start()
+            loadSchedule()
         }
+
         recyclerView = findViewById(R.id.r_recycler)
 
         adapter = ScheduleAdapter(ArrayList(), this)
         recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
         recyclerView.adapter = adapter
-        Thread(ScheduleLoader()).start()
+        loadSchedule()
+        choseDate()
+    }
+
+    private fun choseDate() {
+        var vis = true
+        calendarButton.setOnClickListener {
+            when (datePicker.visibility) {
+                View.VISIBLE -> vis = true
+                View.GONE -> vis = false
+            }
+            if (vis == false) {
+                datePicker.visibility = View.VISIBLE
+            } else {
+                datePicker.visibility = View.GONE
+                setData()
+            }
+        }
     }
 }
