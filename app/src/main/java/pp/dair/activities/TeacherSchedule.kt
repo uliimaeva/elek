77fe4040ccc2f2020ed3987ae5c90abc477585f1
uli <i.julka14@yandex.ru>
2024-03-05 16:58:49 +0300

    teacher schedule

--- app/src/main/java/pp/dair/activities/TeacherSchedule.kt
+++ app/src/main/java/pp/dair/activities/TeacherSchedule.kt
@@ -1,12 +1,209 @@
 package pp.dair.activities
 
+import android.annotation.SuppressLint
+import android.app.NotificationManager
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.DatePicker
+import android.widget.ImageButton
+import android.widget.TextView
+import android.widget.Toast
+import androidx.core.app.NotificationCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.navigation.NavigationView
 import pp.dair.R
+import pp.dair.adapters.ScheduleAdapter
+import pp.dair.adapters.ScheduleTeacherAdapter
+import pp.dair.models.LessonWithGroup
+import pp.dair.models.LessonWithMark
+import pp.dair.viewmodels.StaffViewModel
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import java.util.Calendar
 
 class TeacherSchedule : AppCompatActivity() {
+
+    private  var viewModel: StaffViewModel = StaffViewModel()
+    private lateinit var recyclerView: RecyclerView
+    lateinit var adapter: ScheduleTeacherAdapter
+    private val calendar = Calendar.getInstance()
+    lateinit var header: TextView
+    private lateinit var left_button: ImageButton
+    private lateinit var  right_button: ImageButton
+    private lateinit var journalButton: ImageButton
+    private lateinit var datePicker: DatePicker
+    private lateinit var calendarButton: ImageButton
+    private lateinit var checkButton: ImageButton
+
+    var id = 0
+    var day = calendar.get(Calendar.DAY_OF_MONTH)
+    var year = calendar.get(Calendar.YEAR)
+    var month = calendar.get(Calendar.MONTH) + 1
+    var day_of_year = calendar.get(Calendar.DAY_OF_YEAR)
+
+
+    fun numberToWeekDay(value: Int): String {
+        return when (value) {
+            Calendar.MONDAY -> "Понедельник"
+            Calendar.TUESDAY -> "Вторник"
+            Calendar.WEDNESDAY -> "Среда"
+            Calendar.THURSDAY -> "Четверг"
+            Calendar.FRIDAY -> "Пятница"
+            Calendar.SATURDAY -> "Суббота"
+            Calendar.SUNDAY -> "Воскресенье"
+            else -> throw Exception("Нет такого дня недели")
+        }
+    }
+
+    fun showToast(text: String) {
+        val builder: NotificationCompat.Builder = NotificationCompat.Builder(this)
+            .setSmallIcon(R.mipmap.ic_launcher)
+            .setContentTitle("Title")
+            .setContentText("Notification text")
+
+        val notification = builder.build()
+
+        val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.notify(1, notification)
+        Toast.makeText(this, text, Toast.LENGTH_LONG).show()
+    }
+
+    fun setData() {
+        val today = Calendar.getInstance()
+        datePicker.init(
+            today.get(Calendar.YEAR), today.get(Calendar.MONTH),
+            today.get(Calendar.DAY_OF_MONTH)
+
+        ) { _, year, month, day ->
+            val month = month + 1
+            calendar.set(datePicker.year, datePicker.month, datePicker.dayOfMonth)
+            header.text = String.format(
+                "%d.%d.%d %s",
+                day,
+                month,
+                year,
+                numberToWeekDay(calendar.get(Calendar.DAY_OF_WEEK))
+            )
+            this.day = datePicker.dayOfMonth
+            this.year = datePicker.year
+            this.month = datePicker.month + 1
+
+        }
+    }
+
+    fun loadSchedule() {
+        viewModel.getTeacherSchedule(id, year, month, day, object : Callback<ArrayList<LessonWithGroup>> {
+            override fun onResponse(
+                call: Call<ArrayList<LessonWithGroup>>,
+                response: Response<ArrayList<LessonWithGroup>>,
+            ) {
+                if (response.isSuccessful) {
+                    Log.d("HUI", "Расписание получено!")
+                    for (lesson in response.body()!!) {
+                        Log.d("HUI", lesson.toString())
+                    }
+                    adapter.setArray(response.body()!!)
+                } else {
+                    showToast("Технические шоколадки 2")
+                    adapter.setArray(ArrayList())
+                }
+            }
+
+            override fun onFailure(call: Call<ArrayList<LessonWithGroup>>, t: Throwable) {
+                adapter.setArray(ArrayList())
+                showToast("Технические шоколадки!")
+            }
+        })
+    }
+
+    fun setMenuData(){
+
+    }
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_teacher_schedule)
+
+        val navigationView = findViewById<NavigationView>(R.id.navigationView)
+        recyclerView = findViewById(R.id.t_sheadule)
+
+        adapter = ScheduleTeacherAdapter(ArrayList(), this, this)
+        recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        recyclerView.adapter = adapter
+
+        header.text = String.format(
+            "%d.%d.%d %s",
+            day,
+            month,
+            year,
+            numberToWeekDay(calendar.get(Calendar.DAY_OF_WEEK))
+        )
+
+        left_button.setOnClickListener {
+            calendar.add(Calendar.DAY_OF_MONTH, -1)
+            this.day = calendar.get(Calendar.DAY_OF_MONTH)
+            this.year = calendar.get(Calendar.YEAR)
+            this.month = calendar.get(Calendar.MONTH) + 1
+            header.text = String.format(
+                "%d.%d.%d %s",
+                day,
+                month,
+                year,
+                numberToWeekDay(calendar.get(Calendar.DAY_OF_WEEK))
+            )
+            loadSchedule()
+        }
+        right_button.setOnClickListener {
+            calendar.add(Calendar.DAY_OF_MONTH, 1)
+            this.day = calendar.get(Calendar.DAY_OF_MONTH)
+            this.year = calendar.get(Calendar.YEAR)
+            this.month = calendar.get(Calendar.MONTH) + 1
+            header.text = String.format(
+                "%d.%d.%d %s",
+                day,
+                month,
+                year,
+                numberToWeekDay(calendar.get(Calendar.DAY_OF_WEEK))
+            )
+
+            loadSchedule()
+        }
+
+        calendarButton.setOnClickListener {
+            datePicker.visibility = View.VISIBLE
+            checkButton.visibility = View.VISIBLE
+            calendarButton.visibility = View.GONE
+            setData()
+        }
+        checkButton.setOnClickListener {
+            datePicker.visibility = View.GONE
+            checkButton.visibility = View.GONE
+            calendarButton.visibility = View.VISIBLE
+            loadSchedule()
+        }
+
+        val navListener = NavigationView.OnNavigationItemSelectedListener { item ->
+
+            when (item.itemId) {
+                R.id.p_Back -> {
+                    startActivity(Intent(this, JournalActivity::class.java))
+                }
+                R.id.p_Exit -> {
+                    val manager = supportFragmentManager
+                    val myDialogFragment = MyDialogFragment()
+                    myDialogFragment.show(manager, "myDialog")
+                }
+
+            }
+            true
+        }
+        navigationView.setNavigationItemSelectedListener(navListener)
     }
+
 }
---
+++ app/src/main/java/pp/dair/adapters/ScheduleTeacherAdapter.kt
@@ -0,0 +1,77 @@
+package pp.dair.adapters
+
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.RelativeLayout
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat.getDrawable
+import androidx.recyclerview.widget.RecyclerView
+import pp.dair.R
+import pp.dair.activities.ScheduleDialogFragment
+import pp.dair.models.LessonWithGroup
+import pp.dair.models.LessonWithMark
+import pp.dair.retrofit.Common
+
+
+class ScheduleTeacherAdapter(
+    private var scheduleArray: ArrayList<LessonWithGroup>,
+    private var activity: Activity,
+    private var context: Context
+): RecyclerView.Adapter<ScheduleTeacherAdapter.MyViewHolder>() {
+
+    private var listener: (() -> Unit)? = null
+    fun setListener(listener: (() -> Unit)?) {
+        this.listener = listener
+    }
+
+    class MyViewHolder(item: View) : RecyclerView.ViewHolder(item) {
+        val subject: TextView = itemView.findViewById(R.id.sub_name)
+        val group: TextView = itemView.findViewById(R.id.group)
+        val aud: TextView = itemView.findViewById(R.id.aud)
+        val mainLayout: RelativeLayout = item.findViewById(R.id.mainLayout)
+    }
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.shedule_recycler_teacher, parent, false)
+        return MyViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        var subject_header = scheduleArray[position].number.toString() + ") " + scheduleArray[position].subject
+        if (scheduleArray[position].subgroup != 0) {
+            subject_header += String.format(" (%d п/г)", scheduleArray[position].subgroup)
+        }
+        holder.subject.text = subject_header
+        holder.aud.text = scheduleArray[position].audience
+        holder.group.text = scheduleArray[position].group
+
+//
+//        holder.mainLayout.setOnClickListener(View.OnClickListener {
+//
+//            val scheduleDialogFragment = ScheduleDialogFragment()
+//            val manager = (context as AppCompatActivity).supportFragmentManager;
+//            scheduleDialogFragment.show(manager, "scheduleDialog")
+//            Common.currentSub = scheduleArray[position]
+//            listener?.invoke()
+//        })
+    }
+
+
+    override fun getItemCount(): Int {
+        return scheduleArray.size
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    fun setArray(array: ArrayList<LessonWithGroup>) {
+        this.scheduleArray = array
+        notifyDataSetChanged()
+    }
+}
