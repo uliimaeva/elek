72cdac2a20c109acc36897413598afc2f6c2dc81
uliimaeva <i.julka14@yandex.ru>
2024-04-03 19:37:39 +0300

    feat: added hours adapter

--- app/src/main/java/pp/dair/activities/HoursActivity.kt
+++ app/src/main/java/pp/dair/activities/HoursActivity.kt
@@ -2,10 +2,21 @@ package pp.dair.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.ImageButton
 import android.widget.TextView
+import androidx.core.content.ContentProviderCompat.requireContext
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import pp.dair.R
+import pp.dair.adapters.HoursAdapter
+import pp.dair.models.Staff
+import pp.dair.models.TeacherLoad
+import pp.dair.models.TeacherLoadResponse
+import pp.dair.viewmodels.StaffViewModel
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import java.util.Calendar
 
 class HoursActivity : AppCompatActivity() {
@@ -15,7 +26,9 @@ class HoursActivity : AppCompatActivity() {
     lateinit var rightButton: ImageButton
     lateinit var header: TextView
     private var calendar = Calendar.getInstance()
+    private var viewModel: StaffViewModel = StaffViewModel()
     var month = calendar.get(Calendar.MONTH) + 1
+    private val adapter = HoursAdapter(emptyMap(), this, this)
 
     fun monthName(value: Int): String {
         return when (value) {
@@ -35,11 +48,32 @@ class HoursActivity : AppCompatActivity() {
         }
     }
 
+    fun loadLoad() {
+        viewModel.getTeacherLoad(calendar.get(Calendar.YEAR), month, object: Callback<TeacherLoadResponse> {
+            override fun onResponse(
+                call: Call<TeacherLoadResponse>,
+                response: Response<TeacherLoadResponse>,
+            ) {
+                if (response.isSuccessful) {
+                    adapter.setTotalMap(response.body()!!.total)
+                }
+            }
+
+            override fun onFailure(call: Call<TeacherLoadResponse>, t: Throwable) {
+                Log.d("ERROR", t.toString())
+            }
+
+        })
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_hours)
 
         recyclerView = findViewById(R.id.h_recycler)
+        recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        recyclerView.adapter = adapter
+
         header = findViewById(R.id.r_date)
         leftButton = findViewById(R.id.left)
         rightButton = findViewById(R.id.right)
@@ -54,7 +88,7 @@ class HoursActivity : AppCompatActivity() {
             header.text = String.format(
                 "%s",monthName(month)
             )
-//            loadSchedule()
+            loadLoad()
         }
         rightButton.setOnClickListener {
             calendar.add(Calendar.MONTH, 1)
@@ -62,9 +96,9 @@ class HoursActivity : AppCompatActivity() {
             header.text = String.format(
                 "%s",monthName(month)
             )
-//            loadSchedule()
+            loadLoad()
         }
-
+        loadLoad()
 
     }
 }
--- app/src/main/java/pp/dair/adapters/HoursAdapter.kt
+++ app/src/main/java/pp/dair/adapters/HoursAdapter.kt
@@ -11,25 +11,37 @@ import pp.dair.R
 import pp.dair.models.TeacherLoad
 
 class HoursAdapter(
-    private var hoursMap: Map<String, ArrayList<TeacherLoad>>,
+    private var totalMap: Map<String, TeacherLoad>,
     private val activity: Activity,
     private val context: Context,
 ): RecyclerView.Adapter<HoursAdapter.MyVewHolder>() {
 
     class MyVewHolder(item: View): RecyclerView.ViewHolder(item) {
+        val group: TextView = itemView.findViewById(R.id.group)
+        val subject: TextView = itemView.findViewById(R.id.subject)
+        val readHours: TextView = itemView.findViewById(R.id.read_hours)
+        val remainHours: TextView = itemView.findViewById(R.id.remaining_hours)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyVewHolder {
-        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.hours_recycler)
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.hours_recycler, parent, false)
         return MyVewHolder(itemView)
     }
 
     override fun onBindViewHolder(holder: HoursAdapter.MyVewHolder, position: Int) {
-
+        val mapList = totalMap.toList()
+        holder.group.text = mapList[position].second.group
+        holder.subject.text = mapList[position].second.subject
+        holder.readHours.text = mapList[position].second.hours.toString()
+        holder.remainHours.text = "-"
     }
 
     override fun getItemCount(): Int {
-        return
+        return totalMap.size
     }
 
+    fun setTotalMap(total: Map<String, TeacherLoad>) {
+        totalMap = total
+        notifyDataSetChanged()
+    }
 }
