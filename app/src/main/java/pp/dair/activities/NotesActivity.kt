b8ee771f4483bd2c6be5504474e156d9ca5cf974
uli <i.julka14@yandex.ru>
2024-05-27 10:17:01 +0300

    (⌐■_■) dark mode

--- app/src/main/java/pp/dair/activities/NotesActivity.kt
+++ app/src/main/java/pp/dair/activities/NotesActivity.kt
@@ -1,6 +1,7 @@
 package pp.dair.activities
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -10,6 +11,7 @@ import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager.widget.ViewPager
+import com.google.android.material.navigation.NavigationView
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 import pp.dair.PageAdapter
@@ -39,6 +41,41 @@ class NotesActivity : AppCompatActivity() {
 
         tabLayout = findViewById(R.id.tabLayout)
         tabLayout.setupWithViewPager(viewPager)
+
+        val navigationView = findViewById<NavigationView>(R.id.navigationView)
+
+
+        val navListener = NavigationView.OnNavigationItemSelectedListener { item ->
+
+            when (item.itemId) {
+                R.id.p_teacher -> {
+                    startActivity(Intent(this, TeacherSchedule::class.java))
+                }
+                R.id.p_groups -> {
+                    startActivity(Intent(this, GroupSchedule::class.java))
+                }
+                R.id.p_hours -> {
+                    startActivity(Intent(this, HoursActivity::class.java))
+                }
+
+                R.id.p_Note -> {
+                    startActivity(Intent(this, NotesActivity::class.java))
+                }
+
+                R.id.p_ExitProfile -> {
+                    startActivity(Intent(this, AuthActivity::class.java))
+                }
+
+                R.id.p_Exit -> {
+                    val manager = supportFragmentManager
+                    val myDialogFragment = MyDialogFragment()
+                    myDialogFragment.show(manager, "myDialog")
+                }
+            }
+            true
+        }
+
+        navigationView.setNavigationItemSelectedListener(navListener)
 
     }
 }
